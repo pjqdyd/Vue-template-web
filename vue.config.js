71ca162d7574e6e30760b6c967c697b34506102f
pjqdyd <1909025079@qq.com>
2019-10-24 19:18:52 +0800

    部分使用cdn加载依赖,防止打包的js文件过大

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+  // 项目部署的基础路径
+  // 我们默认假设你的应用将会部署在域名的根部,
+  // 例如 https://www.my-app.com/
+  // 如果你的应用部署在一个子路径下，那么你需要在这里
+  // 指定子路径。比如将你的应用部署在
+  // https://www.foobar.com/my-app/
+  // 那么将这个值改为 '/my-app/'
+  baseUrl: "/",
+  outputDir: "dist", // where to put static assets (js/css/img/font/...) 
+  lintOnSave: true,
+  runtimeCompiler: false,
+  transpileDependencies: [],
+
+  productionSourceMap: false, // 调整内部的webpack配置.
+    configureWebpack: {
+        externals: {
+            'vue': 'Vue',
+            'vuex': 'Vuex',
+            'vue-router': 'VueRouter',
+            'vue-i18n': 'VueI18n',
+            'axios':'axios',
+            'element-ui': 'ELEMENT'
+        },
+      plugins: [
+        // ...
+      ]
+    }
+  }
